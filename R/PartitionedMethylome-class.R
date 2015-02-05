74d512ea3acc340f61de9c07a5b6408d5758ae84
Peter Hickey <peter.hickey@gmail.com>
2015-02-05 16:40:58 +1100

    Simplified partitionMethylome and created PartitionedMethylome class.

---
+++ R/PartitionedMethylome-class.R
@@ -0,0 +1,52 @@
+### =========================================================================
+### PartitionedMethylome: An S4 class to formalise an informal GRanges-based
+### class used by MethylSeekR.
+### -------------------------------------------------------------------------
+###
+
+#' @export
+setClass("PartitionedMethylome",
+         contains = "GRanges"
+)
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Validity
+###
+
+
+.valid.PartitionedMethylome.mcols <- function(object) {
+  msg <- NULL
+  if (!is.factor(mcols(object)$type) ||
+        !identical(levels(mcols(object)$type), c("UMR", "LMR", "PMR", "other"))) {
+    msg <- validMsg(msg, paste0("'type' must be a factor with levels ",
+                                "'UMR', 'LMR', 'PMR' and 'other'."))
+  }
+  msg
+}
+
+.valid.PartitionedMethylome.GRanges <- function(object) {
+  msg <- NULL
+  if (!isDisjoint(object)) {
+    msg <- validMsg(msg, "'PartitionedMethylome' must be disjoint.")
+  }
+  gaps <- gaps(granges(object))
+  gaps <- gaps[strand(gaps) == "*"]
+  if (length(gaps)) {
+    msg <- validMsg(msg, "'PartitionedMethylome' must not contain gaps.")
+  }
+  msg
+}
+
+.valid.PartitionedMethylome <- function(object) {
+  # Include all .valid.PartitionedMethylome.* functions in this vector
+  msg <- c(.valid.PartitionedMethylome.mcols(object),
+           .valid.PartitionedMethylome.GRanges(object))
+
+  if (is.null(msg)){
+    return(TRUE)
+  } else{
+    msg
+  }
+}
+
+S4Vectors::setValidity2("PartitionedMethylome", .valid.PartitionedMethylome)
--- R/partitionMethylome.R
+++ R/partitionMethylome.R
@@ -3,35 +3,46 @@
 ### -------------------------------------------------------------------------
 ###
 
-#' Partition a methylome into UMRs, LMRs, PMRs and MMRs.
+# TODO: Create the PartionedMethylome class and return an object of this class.
+#' Partition a methylome into UMRs, LMRs, PMRs and others.
 #'
 #' Paritition a methylome into unmethylated regions (UMRs), lowly-methylated
 #' regions (LMRs), partially-methylated regions (PMRs) and mostly-methylated
-#' regions (MMRs) based on the beta-values.
-#'
-#' This function is a convenience function wrapping around several functions
-#' provided by the \code{MethylSeekR} Bioconductor package. It is not a
-#' replacement.
+#' regions (others) based on the beta-values.
 #'
 #' @param umrs_lmrs The output of
-#' \code{MethylSeekR\link[MethylSeekR]{segmentUMRsLMRs}}.
+#' \code{MethylSeekR::\link[MethylSeekR]{segmentUMRsLMRs}}.
 #' @param pmrs The output of \code{MethylSeekR\link[MethylSeekR]{segmentPMDs}}.
+#' @param bsgenome A \code{BSgenome::\link[BSgenome]{BSgenome}} object of the
+#' relevant organism.
 #' @return A \code{\link{PartitionedMethylome}}.
 #' @export
 partitionMethylome <- function(umrs_lmrs, pmrs) {
 
   if (!isDisjoint(umrs_lmrs)) {
-    stop("Expect 'umrs_lmrs' to be disjoint. Please report this error.")
+    stop("Expected 'umrs_lmrs' to be disjoint. Please report this error.")
   }
   if (!isDisjoint(pmrs)) {
-    stop("Expect 'pmrs' to be disjoint. Please report this error.")
+    stop("Expected 'pmrs' to be disjoint. Please report this error.")
   }
 
   m <- c(granges(umrs_lmrs, use.mcols = FALSE),
          granges(pmrs, use.mcols = FALSE))
   m$type <- c(umrs_lmrs$type, pmrs$type)
-  m <- sort(m)
-  m_disjoint <- disjoin(m)
-  mo_idx <- findMostOverlapping(m, m_disjoint)
-  # UP TO HERE
+  pm <- sort(m)
+  pm <- disjoin(m)
+  # Get the typo of each range in the partitioned methylome
+  type <- rep(NA_character_, length(pm))
+  # Get the type of those with ranges with exact matches in m
+  equal_match_ol <- findOverlaps(pm, m, type = "equal")
+  type[queryHits(equal_match_ol)] <- mcols(m)$type[subjectHits(equal_match_ol)]
+  # Get the type of those ranges that overlap both UMR/LMR and PMR
+  mo_idx <- findMostOverlapping(pm[-queryHits(equal_match_ol)], m)
+  type[-queryHits(equal_match_ol)] <- mcols(m)$type[mo_idx]
+  type <- gsub("notPMD", "other", type)
+  type <- gsub("PMD", "PMR", type)
+  type <- factor(type, levels = c("UMR", "LMR", "PMR", "other"))
+  mcols(pm) <- DataFrame(type = type)
+  seqlevels(pm) <- seqlevelsInUse(pm)
+  new("PartitionedMethylome", pm)
 }
