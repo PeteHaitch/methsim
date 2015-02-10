f871e1220fc9ac5de3c9a20d51ac4c7fc1cf6712
Peter Hickey <peter.hickey@gmail.com>
2015-02-10 15:35:20 +1100

    Added plotAlphaDistributionOneChr() utility function

---
+++ R/utils.R
@@ -0,0 +1,32 @@
+### =========================================================================
+### Utility functions. These functions are not exported.
+### -------------------------------------------------------------------------
+###
+
+# A copy of MethylSeekR::plotAlphaDistributionOneChr but does some
+# parallelisation by sample.
+# TODO: Document and robust-ify (long-term)
+plotAlphaDistributionOneChr <- function(l_msrgr, sn, chr.sel, ncol, nrow,
+                                        nCGbin = 101) {
+  message("determining alpha distribution for chromosome: ",
+          chr.sel)
+  l_indx <- bplapply(l_msrgr, function(msrgr, chr.sel) {
+    indx <- seqnames(msrgr) == chr.sel
+    if (sum(indx) < nCGbin) {
+      stop(sprintf("Error: less than %d covered CpGs on chromosome %s",
+                   nCGbin, chr.sel))
+    }
+    indx
+  }, chr.sel = chr.sel)
+  l_score <- bpmapply(function(m, indx) {
+    T <- as.numeric(values(m[indx])[, 1])
+    M <- as.numeric(values(m[indx])[, 2])
+    MethylSeekR:::calculateAlphaDistr(M, T, nCGbin, num.cores = 1)
+  }, m = l_msrgr, indx = l_indx)
+  par(mfrow = c(nrow, ncol))
+  mapply(function(score, sn, chr.sel) {
+    hist(score, probability = TRUE, breaks = 30,
+         xlab = sprintf("posterior mean of alpha (%s)", chr.sel), main = sn)
+  }, score = l_score, sn = sn, MoreArgs = list(chr.sel = chr.sel))
+  return(NULL)
+}
