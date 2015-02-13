a38f39815b45a7e514b494453f40b5f61ecc2467
Peter Hickey <peter.hickey@gmail.com>
2015-02-13 12:14:29 +1100

    Added plotFinalSegmentation() utility function

--- R/utils.R
+++ R/utils.R
@@ -21,34 +21,6 @@ plotAlphaDistributionOneChr <- function(m, chr.sel, nCGbin = 101,
        xlab = sprintf("posterior mean of alpha (%s)", chr.sel), plot)
 }
 
-# A copy of MethylSeekR::plotAlphaDistributionOneChr but does some
-# parallelisation by sample.
-# TODO: Deprecate
-plotAlphaDistributionOneChr_2 <- function(l_msrgr, sn, chr.sel, ncol, nrow,
-                                          nCGbin = 101) {
-  message("determining alpha distribution for chromosome: ",
-          chr.sel)
-  l_indx <- bplapply(l_msrgr, function(msrgr, chr.sel) {
-    indx <- seqnames(msrgr) == chr.sel
-    if (sum(indx) < nCGbin) {
-      stop(sprintf("Error: less than %d covered CpGs on chromosome %s",
-                   nCGbin, chr.sel))
-    }
-    indx
-  }, chr.sel = chr.sel)
-  l_score <- bpmapply(function(m, indx) {
-    T <- as.numeric(values(m[indx])[, 1])
-    M <- as.numeric(values(m[indx])[, 2])
-    MethylSeekR:::calculateAlphaDistr(M, T, nCGbin, num.cores = 1L)
-  }, m = l_msrgr, indx = l_indx)
-  par(mfrow = c(nrow, ncol))
-  mapply(function(score, sn, chr.sel) {
-    hist(score, probability = TRUE, breaks = 30,
-         xlab = sprintf("posterior mean of alpha (%s)", chr.sel), main = sn)
-  }, score = l_score, sn = sn, MoreArgs = list(chr.sel = chr.sel))
-  return(NULL)
-}
-
 # A copy of MethylSeekR::segmentPMDs that can be safely run in
 # parallel because it does not create the plot but returns it as an object.
 # TODO: Document and robust-ify (long-term)
@@ -273,4 +245,87 @@ segmentUMRsLMRs <- function(m, meth.cutoff = 0.5, nCpG.cutoff = 3, PMDs = NA,
        ylab = "median methylation (%)",
        v = log2(nCG.classification),
        segments.gr = segments.gr)
+}
+
+# A copy of MethylSeekR::plotFinalSegmentation that can be safely run in
+# parallel because it does not create the plot but returns it as an object.
+# TODO: Document and robust-ify (long-term)
+plotFinalSegmentation <- function(m, sn, segs, PMDs = NA, meth.cutoff,
+                                  numRegions = 1, minCover = 5,
+                                  nCpG.smoothing = 3, plot = FALSE) {
+  m = m[values(m)[, 1] >= minCover]
+  cols.seg = c("#377EB8", "#E41A1C")
+  names(cols.seg) = c("UMR", "LMR")
+  col.PMD = "#4DAF4A"
+  pch.seg <- c(LMR = 24, UMR = 22)
+  chrs = unique(as.character(seqnames(m)))
+  height = 1
+  len = 5000
+  nCGperChr = table(seqnames(m))
+  indx = which(nCGperChr[chrs] < 3 * len)
+  if (length(indx) > 0) {
+    chrs = chrs[-indx]
+  }
+  exist.PMDs <- FALSE
+  if (class(PMDs) == "GRanges") {
+    exist.PMDs <- TRUE
+  }
+  for (ii in 1:numRegions) {
+    chr = sample(chrs, 1)
+    i = seqnames(m) == chr
+    s = sample(1:(length(m[i]) - 3 * len), 1)
+    par(mfrow = c(6, 1))
+    for (gg in 1:3) {
+      indx = (s + (gg - 1) * len):(s + (gg) * len)
+      seg.sel = subsetByOverlaps(segs, m[i][indx])
+      if (length(seg.sel) > 0) {
+        mysegcol <- cols.seg[as.character(values(seg.sel)$type)]
+        mysegpch <- pch.seg[as.character(values(seg.sel)$type)]
+      }
+      PMD.sel = NA
+      if (exist.PMDs) {
+        PMD.sel = subsetByOverlaps(PMDs[values(PMDs)$type == "PMD"], m[i][indx])
+      }
+      add.mar <- 2
+      par(mar = c(3 - add.mar, 4, 1 + add.mar, 2))
+      plot(start(m[i])[indx], 100 * values(m[i])[indx, 2] /
+             values(m[i])[indx, 1], pch = "*", ylim = c(-18, 100), cex = 0.9,
+           ylab = "methylation", axes = FALSE, xlab = "", main = sn)
+      axis(2, at = c(0, 50, 100))
+      box()
+      text(par("usr")[1] + par("cxy")[1] * 0.2, par("usr")[3] +
+             par("cxy")[2] * 0.3, adj = c(0, 0), label = "raw")
+      if (exist.PMDs & length(PMD.sel) > 0) {
+        rect(start(PMD.sel), -18 - height / 4, end(PMD.sel),
+             -18 + height / 4, lwd = 2, col = col.PMD, border = col.PMD)
+      }
+      if (length(seg.sel) > 0) {
+        points(x = mid(ranges(seg.sel)),
+               y = rep(-18, length(seg.sel)), pch = mysegpch, cex = 1.3,
+               col = "black", lwd = 0.5, bg = mysegcol)
+      }
+      par(mar = c(2 + add.mar, 4, 2 - add.mar, 2))
+      plot(start(m[i])[indx], 100 *
+             as.vector(runmean(Rle(values(m[i])[indx, 2] /
+                                     values(m[i])[indx, 1]),
+                               nCpG.smoothing, endrule = "constant")),
+           pch = "*", ylim = c(-18, 100), cex = 0.9, ylab = "methylation",
+           xlab = sprintf("position on %s", chr), axes = FALSE)
+      axis(1)
+      axis(2, at = c(0, 50, 100))
+      box()
+      text(par("usr")[1] + par("cxy")[1] * 0.2, par("usr")[3] +
+             par("cxy")[2] * 0.3, adj = c(0, 0), label = "smoothed")
+      if (exist.PMDs & length(PMD.sel) > 0) {
+        rect(start(PMD.sel), -18 - height / 4, end(PMD.sel),
+             -18 + height / 4, lwd = 2, col = col.PMD, border = col.PMD)
+      }
+      if (length(seg.sel) > 0) {
+        points(x = mid(ranges(seg.sel)),
+               y = rep(-18, length(seg.sel)), pch = mysegpch, cex = 1.3,
+               col = "black", lwd = 0.5, bg = mysegcol)
+      }
+      abline(h = 100 * meth.cutoff, lty = 5, col = "darkgrey")
+    }
+  }
 }
