c300bedd5b2c4d78198bbb45e4f235dd756ef1e4
Peter Hickey <peter.hickey@gmail.com>
2015-03-11 13:39:15 +1100

    Added design for addDMRs method.

---
+++ R/addDMRs.R
@@ -0,0 +1,18 @@
+### =========================================================================
+### addDMRs: A method to add differentially methylated regions to a
+### SimulatedMethylome.
+### -------------------------------------------------------------------------
+###
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Design
+###
+### This should return a suitably modified SimulatedMethylome object and some
+### information on the DMRs (e.g., location, effect size).
+###
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### TODO
+###
+### This is a longer term goal that I haven't yet figured out how best to
+### implement. The key question is how to control the effect size.
