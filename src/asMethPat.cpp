ce693396dd055078f33bcae51c8f9e4f3df2e233
Peter Hickey <peter.hickey@gmail.com>
2015-03-18 15:07:16 +1100

    Updated comments for .asMethPat().

--- src/asMethPat.cpp
+++ src/asMethPat.cpp
@@ -2,7 +2,11 @@
 using namespace Rcpp;
 
 // WARNING: This will produce zero-rows, i.e., unobserved m-tuples.
+// WARNIGN: This should only be used for size >= 2.
 
+// TODO: Document
+// TODO: Re-name to better reflect its function.
+// TODO: What happens if zero-length vectors are passed to it.
 // [[Rcpp::export(".asMethPat")]]
 std::map<std::string, std::vector<int> > asMethPat(IntegerVector readID,
                                            IntegerVector z,
@@ -22,10 +26,7 @@ std::map<std::string, std::vector<int> > asMethPat(IntegerVector readID,
   std::stringstream mtuples_key;
   std::vector<int> mtuples_value(pow(2, size));
 
-  // TODO: Do I need a corresponding map for the positions themselves?
-
-
-  // Initialise the map of all possible adjacent m-tuples
+  // Initialise the map of all adjacent m-tuples
   IntegerVector unique_pos = clone(unique(pos));
   std::sort(unique_pos.begin(), unique_pos.end());
   int n = unique_pos.length() - size + 1;
@@ -42,11 +43,9 @@ std::map<std::string, std::vector<int> > asMethPat(IntegerVector readID,
   // Fill the map with the counts of each methylation pattern
   int k = 0;
   int N = readID.length() - size + 1;
-//   Rcpp::Rcout << "N = " << N << std::endl;
   int idx;
   int idx0 = (pow(2, size) - 1);
   while (k < N) {
-    // Rcpp::Rcout << "k = " << k << std::endl;
     // Check that these 'size' positions are from the same read.
     if (readID[k] == readID[k + size - 1]) {
       // idx converts the methylation pattern, e.g., (1, 1), to the index of
@@ -55,22 +54,20 @@ std::map<std::string, std::vector<int> > asMethPat(IntegerVector readID,
       for (int l = k; l < (k + size); l++) {
         idx -= z[l] * pow(2, size - (l - k) - 1);
       }
-      // Rcpp::Rcout << "idx = " << idx << std::endl;
       mtuples_key << pos[k] << ",";
       for (int m = (k + 1); m < (k + size - 1); m++) {
         mtuples_key << pos[m] << ",";
       }
       mtuples_key << pos[(k + size - 1)];
-      // Rcpp::Rcout << "mtuples_key = " << mtuples_key.str() << std::endl;
       mtuples[mtuples_key.str()][idx] += 1;
       mtuples_key.str(std::string());
       k += 1;
     } else {
-      // TODO: Check this is the correct increment when size > 2
+      // Can jump the k-index because we know if the read doesn't contain a
+      // tuple then the next positions in the read can't either.
       k += (size - 1);
     }
   }
 
-  // Rcpp::Rcout << "Final k = " << k << std::endl;
   return mtuples;
 }
